2dcb9448a265807cc4e7dbc01f8f3196c9ae2c0d
Valentin Funk <funk.valentin@gmail.com>
2014-06-20 18:00:57 +0200

    Made Trail not appear on dead players

--- lua/kinv/items/pointshop/sh_base_trail.lua
+++ lua/kinv/items/pointshop/sh_base_trail.lua
@@ -10,28 +10,40 @@ function ITEM:initialize( )
 	print( "CONSTRUCT: Item Trail" )
 end
 
-/*
-	Inventory icon
-*/
-function ITEM:getIcon( )
-	self.icon = vgui.Create( "DPointshopTrailInvIcon" )
-	self.icon:SetItem( self )
-	self.icon:SetSize( 64, 64 )
-	return self.icon
-end
-
-function ITEM:OnEquip( ply )
+function ITEM:AttachTrail( ply )
 	if SERVER then
 		self.trailEnt = util.SpriteTrail( ply, 0, self.color, false, 15, 1, 4, 0.125, self.material .. ".vmt" )
 	end
-	print( "Trail: OnEquip", self.trailEnt )
 end
 
-function ITEM:OnHolster( ply )
-	print( "Trail: OnHolster", self.trailEnt )
+function ITEM:RemoveTrail( )
 	if SERVER then
 		SafeRemoveEntity( self.trailEnt )
 	end
+end
+
+function ITEM:OnEquip( ply )
+	if ply:Alive( ) then
+		self:PlayerSpawn( ply )
+	end
+end
+
+function ITEM:PlayerSpawn( ply )
+	if ply == self:GetOwner( ) then
+		self:AttachTrail( ply )
+	end
+end
+Pointshop2.AddItemHook( "PlayerSpawn", ITEM )
+
+function ITEM:PlayerDeath( victim, inflictor, attacker )
+	if victim == self:GetOwner( ) then
+		self:RemoveTrail( )
+	end
+end
+Pointshop2.AddItemHook( "PlayerSpawn", ITEM )
+
+function ITEM:OnHolster( ply )
+	self:RemoveTrail( )
 end
 
 function ITEM.static:GetPointshopIconControl( )
@@ -49,4 +61,14 @@ end
 
 function ITEM.static.GetPointshopIconDimensions( )
 	return 74, 64
+end
+
+/*
+	Inventory icon
+*/
+function ITEM:getIcon( )
+	self.icon = vgui.Create( "DPointshopTrailInvIcon" )
+	self.icon:SetItem( self )
+	self.icon:SetSize( 64, 64 )
+	return self.icon
 end
