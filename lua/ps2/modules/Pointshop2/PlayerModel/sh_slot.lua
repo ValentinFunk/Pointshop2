83cc11f67bf028ae7c8d4e86a81f0fa2f72f1f25
root <root@vs8u138.comsitec.de>
2014-07-13 15:51:52 +0200

    Deleted duplicate files from linux messsup

--- lua/ps2/modules/Pointshop2/PlayerModel/sh_slot.lua
+++
@@ -1,4 +0,0 @@
-Pointshop2.AddEquipmentSlot( "Model", function( item )
-	--Check if the item is a playermodel
-	return instanceOf( Pointshop2.GetItemClassByName( "base_playermodel" ), item )
-end )
--- lua/ps2/modules/Pointshop2/Trail/sh_slot.lua
+++
@@ -1,4 +0,0 @@
-Pointshop2.AddEquipmentSlot( "Trail", function( item )
-	--Check if the item is a trail
-	return instanceOf( Pointshop2.GetItemClassByName( "base_trail" ), item )
-end )
