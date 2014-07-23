1af08a2b6bef9d2bd1f4a00a7ed246546d3274ac
Valentin Funk <funk.valentin@gmail.com>
2014-07-23 15:57:20 +0200

    Fix TTT Preview

--- lua/ps2/modules/ttt_integration/cl_tttpreview.lua
+++ lua/ps2/modules/ttt_integration/cl_tttpreview.lua
@@ -1,6 +1,10 @@
 --If player is in spec and has no model equipped use a Terrorist as preview model
 hook.Add( "PS2_GetPreviewModel", "DefaultToTerrorist", function( )
-	if LocalPlayer():IsSpec( ) then
+	if LocalPlayer():IsSpec() then
 		return GAMEMODE.playermodel or "models/player/phoenix.mdl"
 	end
+end )
+
+hook.Add( "TTTPlayerColor", "FixTTTModel", function( )
+	hook.Run( "PS2_DoUpdatePreviewModel" )
 end )
