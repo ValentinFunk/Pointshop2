a9b695ee60a6540a36a2b4a04034b6ae0ba7a124
Kamshak <funk.valentin@gmail.com>
2014-07-26 12:30:18 +0200

    Add PS2_PointsAwarded

--- lua/ps2/shared/sh_playerextension.lua
+++ lua/ps2/shared/sh_playerextension.lua
@@ -23,6 +23,9 @@ end
 
 function Player:PS2_AddStandardPoints( points, message, small )
 	if points == 0 then return end
+	
+	hook.Run( "PS2_PointsAwarded", self, points )
+	
 	Pointshop2Controller:getInstance( ):addToPlayerWallet( self, "points", points )
 	if message then
 		Pointshop2Controller:getInstance( ):addToPointFeed( self, message, points, small )
