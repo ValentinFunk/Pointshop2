641254ea896d9e4b2087b302e8fddae3b1d2f031
Valentin Funk <funk.valentin@gmail.com>
2014-08-02 14:33:29 +0200

    Moved AddStandardPoints to correct realm

--- lua/ps2/shared/sh_playerextension.lua
+++ lua/ps2/shared/sh_playerextension.lua
@@ -20,14 +20,3 @@ end
 function Player:PS2_GetWallet( )
 	return self.PS2_Wallet
 end
-
-function Player:PS2_AddStandardPoints( points, message, small )
-	if points == 0 then return end
-	
-	hook.Run( "PS2_PointsAwarded", self, points )
-	
-	Pointshop2Controller:getInstance( ):addToPlayerWallet( self, "points", points )
-	if message then
-		Pointshop2Controller:getInstance( ):addToPointFeed( self, message, points, small )
-	end
-end
