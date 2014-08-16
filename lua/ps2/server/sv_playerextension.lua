9b96ec9523502dd8def74e73408eeebb6f075fb2
TripsLetash <tibo.titusz@gmail.com>
2014-08-16 19:14:43 +0200

    Added an AddPremiumPoints function
    AddPremiumPoints

--- lua/ps2/server/sv_playerextension.lua
+++ lua/ps2/server/sv_playerextension.lua
@@ -10,3 +10,11 @@ function Player:PS2_AddStandardPoints( points, message, small )
 		Pointshop2Controller:getInstance( ):addToPointFeed( self, message, points, small )
 	end
 end
+
+function Player:PS2_AddPremiumPoints( points)
+	if points == 0 then return end		
+	
+	hook.Run( "PS2_PointsAwarded", self, points )
+	
+	Pointshop2Controller:getInstance( ):addToPlayerWallet( self, "premiumPoints", points )
+end
