15547f6e75f831cbd0fd74ee66ca839342b355b7
Kamshak <funk.valentin@gmail.com>
2014-08-22 11:21:30 +0200

    Credit where credit is due. Fix thanks to Chuck

--- lua/ps2/shared/sh_pacprotect.lua
+++ lua/ps2/shared/sh_pacprotect.lua
@@ -1,4 +1,5 @@
 function Pointshop2.ApplyPacSettings( )
+	// Disallow saving/applying outfits
 	hook.Add("PrePACConfigApply", "Pointshop2CheckPACAccess", function( owner, data, bPac3 )
 		if not IsValid( owner ) or not owner:IsPlayer( ) then
 			return false, "invalid player" 
@@ -11,6 +12,7 @@ function Pointshop2.ApplyPacSettings( )
 		return true, ""
 	end )
 	
+	// Disallow opening the editor
 	hook.Add( "PrePACEditorOpen", "Pointshop2CheckPACAccess", function( ply )
 		if Pointshop2.GetSetting( "Pointshop 2", "BasicSettings.LimitPACAccess" ) then
 			if not PermissionInterface.query( ply, "pointshop2 usepac" ) then
