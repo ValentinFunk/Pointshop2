fc058d23623d57a73bfbc341ce2e108e297ab7f3
Valentin Funk <funk.valentin@gmail.com>
2014-11-21 15:24:14 +0100

    Fix having two different ULX category names

--- lua/ulx/modules/sh/sh_pointshop2.lua
+++ lua/ulx/modules/sh/sh_pointshop2.lua
@@ -3,8 +3,8 @@ if SERVER then
 	ULib.ucl.registerAccess("pointshop2 createitems", ULib.ACCESS_SUPERADMIN, "Permission to create new items", "Pointshop 2" )
 	ULib.ucl.registerAccess("pointshop2 manageusers", ULib.ACCESS_SUPERADMIN, "Permission to manage users(give points, items)", "Pointshop 2" )
 	ULib.ucl.registerAccess("pointshop2 managemodules", ULib.ACCESS_SUPERADMIN, "Permission to manage modules and use settings", "Pointshop 2" )
-	ULib.ucl.registerAccess("pointshop2 exportimport", ULib.ACCESS_SUPERADMIN, "Permission to export/import items", "Pointshop2" )
-	ULib.ucl.registerAccess("pointshop2 manageservers", ULib.ACCESS_SUPERADMIN, "Permission to manage servers", "Pointshop2" )
-	ULib.ucl.registerAccess("pointshop2 reset", ULib.ACCESS_SUPERADMIN, "Permission to reset the shop", "Pointshop2" )
-	ULib.ucl.registerAccess("pointshop2 usepac", ULib.ACCESS_SUPERADMIN, "Permission to use the PAC editor", "Pointshop2" )
+	ULib.ucl.registerAccess("pointshop2 exportimport", ULib.ACCESS_SUPERADMIN, "Permission to export/import items", "Pointshop 2" )
+	ULib.ucl.registerAccess("pointshop2 manageservers", ULib.ACCESS_SUPERADMIN, "Permission to manage servers", "Pointshop 2" )
+	ULib.ucl.registerAccess("pointshop2 reset", ULib.ACCESS_SUPERADMIN, "Permission to reset the shop", "Pointshop 2" )
+	ULib.ucl.registerAccess("pointshop2 usepac", ULib.ACCESS_SUPERADMIN, "Permission to use the PAC editor", "Pointshop 2" )
 end
