580e17ca2f434ac2db60dc7a3d9965cf83eff445
Valentin Funk <funk.valentin@gmail.com>
2014-11-30 11:27:31 +0100

    Add small option to DInfoPanel

--- lua/ps2/client/cl_DInfoPanel.lua
+++ lua/ps2/client/cl_DInfoPanel.lua
@@ -26,10 +26,21 @@ function PANEL:Init( )
 	self.content:Dock( TOP )
 end
 
+function PANEL:SetSmall( small )
+	self.small = small
+end
+
 function PANEL:ApplySchemeSettings( )
-	self.header.title:SetFont( self:GetSkin( ).SmallTitleFont )
-	self.header.title:SetTextColor( self:GetSkin( ).Colours.Label.Bright )
-	self.content.font = self:GetSkin( ).fontName
+	if self.small then
+		self.header.title:SetFont( self:GetSkin( ).fontName )
+		self.header.title:SetTextColor( self:GetSkin( ).Colours.Label.Bright )
+		self.header.icon:SetSize( 16, 16 )
+		self.content.font = "DermaDefault"
+	else
+		self.header.title:SetFont( self:GetSkin( ).SmallTitleFont )
+		self.header.title:SetTextColor( self:GetSkin( ).Colours.Label.Bright )
+		self.content.font = self:GetSkin( ).fontName
+	end
 end
 
 function PANEL:SetInfo( title, description, icon )
