5f0be82b970efeb02fc489710c8fea793877e079
Valentin Funk <funk.valentin@gmail.com>
2015-01-04 16:42:13 +0100

    Bump version

--- lua/autorun/pointshop2_init.lua
+++ lua/autorun/pointshop2_init.lua
@@ -3,9 +3,9 @@ LibK.InitializeAddon{
     author = "Kamshak",                   --Name of the author
     luaroot = "ps2",                      --Folder that contains the client/shared/server structure relative to the lua folder,
 	loadAfterGamemode = false,
-	version = "2.1.1",
+	version = "2.1.2",
 	requires = { "KInventory" }	
 }
 
 LibK.addReloadFile( "autorun/pointshop2_init.lua" )
-KLogf( "Pointshop2 Version %s : %s loaded", "{{ script_id }}", "{{ user_id }}" )
+print( "Pointshop2 Version %s : %s loaded", "{{ script_id }}", "{{ user_id }}" )
