8c48cab2d11e411e45fda28f7ec3897858dffa5a
Valentin Funk <funk.valentin@gmail.com>
2015-03-20 13:43:18 +0100

    Add slot ordering

--- lua/ps2/modules/pointshop2/playermodel/sh_slot.lua
+++ lua/ps2/modules/pointshop2/playermodel/sh_slot.lua
@@ -1,4 +1,4 @@
 Pointshop2.AddEquipmentSlot( "Model", function( item )
 	--Check if the item is a playermodel
 	return instanceOf( Pointshop2.GetItemClassByName( "base_playermodel" ), item )
-end )
+end, 0 )
--- lua/ps2/shared/sh_equipmentslots.lua
+++ lua/ps2/shared/sh_equipmentslots.lua
@@ -1,25 +1,41 @@
 Pointshop2.EquipmentSlots = { } -- { { name = validationFunction }, ... }
+Pointshop2.EquipmentSlotLookup = {}
 
 function Pointshop2.IsItemValidForSlot( item, slotName )
-	return Pointshop2.EquipmentSlots[slotName]( item )
+	return Pointshop2.EquipmentSlotLookup[slotName]( item )
 end
 
-function Pointshop2.AddEquipmentSlot( slotName, func )
-	Pointshop2.EquipmentSlots[slotName] = func
+function Pointshop2.AddEquipmentSlot( slotName, func, order )
+	Pointshop2.EquipmentSlotLookup[slotName] = func
+	table.insert( Pointshop2.EquipmentSlots, {
+		name = slotName,
+		canHoldItem = func,
+		order = order or math.huge
+	} )
+	Pointshop2.EquipmentSlots[#Pointshop2.EquipmentSlots].order2 = #Pointshop2.EquipmentSlots
+	table.sort( Pointshop2.EquipmentSlots, function( a, b )
+		if a.order < b.order then 
+			return true
+		end
+		if a.order > b.order then
+			return false
+		end
+		return a.order2 < b.order2
+	end )
 end
 
 function Pointshop2.IsValidEquipmentSlot( slotName )
-	return Pointshop2.EquipmentSlots[slotName] != nil
+	return Pointshop2.EquipmentSlotLookup[slotName] != nil
 end
 
 if CLIENT then
 	hook.Add( "PS2_PopulateSlots", "AddDefaultSlots", function( slotsLayout )
-		for slotName, acceptFunc in pairs( Pointshop2.EquipmentSlots ) do
+		for k, v in pairs( Pointshop2.EquipmentSlots ) do
 			local slot = slotsLayout:Add( "DPointshopEquipmentSlot" )
-			slot:SetLabel( slotName )
-			slot.CanHoldItem = function( self, item ) return acceptFunc( item ) end
-			if LocalPlayer().PS2_Slots[slotName] then
-				slot:SetItem( LocalPlayer().PS2_Slots[slotName] )
+			slot:SetLabel( v.name )
+			slot.CanHoldItem = function( self, item ) return v.canHoldItem( item ) end
+			if LocalPlayer().PS2_Slots[v.name] then
+				slot:SetItem( LocalPlayer().PS2_Slots[v.name] )
 			end
 		end
 	end )
