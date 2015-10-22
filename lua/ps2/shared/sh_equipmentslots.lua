22deb83ac38890e575dccd179fd74147f669baef
Valentin Funk <funk.valentin@gmail.com>
2015-10-22 18:00:18 +0100

    Fix item slot swapping (by disabling, good enough)

--- lua/ps2/shared/sh_equipmentslots.lua
+++ lua/ps2/shared/sh_equipmentslots.lua
@@ -14,7 +14,7 @@ function Pointshop2.AddEquipmentSlot( slotName, func, order )
 	} )
 	Pointshop2.EquipmentSlots[#Pointshop2.EquipmentSlots].order2 = #Pointshop2.EquipmentSlots
 	table.sort( Pointshop2.EquipmentSlots, function( a, b )
-		if a.order < b.order then 
+		if a.order < b.order then
 			return true
 		end
 		if a.order > b.order then
@@ -29,11 +29,34 @@ function Pointshop2.IsValidEquipmentSlot( slotName )
 end
 
 if CLIENT then
+	function Pointshop2.FindSlotThatContains(item)
+		for slotName, itemInSlot in pairs(LocalPlayer().PS2_Slots) do
+			print(slotName, itemInSlot:GetPrintName(), item:GetPrintName())
+			if itemInSlot == item then
+				return slotName
+			end
+		end
+	end
+
 	hook.Add( "PS2_PopulateSlots", "AddDefaultSlots", function( slotsLayout )
 		for k, v in pairs( Pointshop2.EquipmentSlots ) do
 			local slot = slotsLayout:Add( "DPointshopEquipmentSlot" )
+			slot:SetSlotTable(v)
 			slot:SetLabel( v.name )
-			slot.CanHoldItem = function( self, item ) return v.canHoldItem( item ) end
+			slot.CanHoldItem = function( self, item )
+				-- Handle swaps between two slots correctly
+				local itemInThisSlot = LocalPlayer().PS2_Slots[v.name]
+				local previousSlot = Pointshop2.FindSlotThatContains(item)
+				if itemInThisSlot and previousSlot then
+					-- calls slot's function directly to avoid infinite loops
+					if not Pointshop2.EquipmentSlotLookup[previousSlot]() then
+						return false
+					end
+				end
+
+				-- delegate to slot's function
+				return v.canHoldItem( item )
+			end
 			if LocalPlayer().PS2_Slots[v.name] then
 				slot:SetItem( LocalPlayer().PS2_Slots[v.name] )
 			end
