fdf6ea7641c0a87b6bfa123fad37d49fc1a5f5fd
Valentin Funk <funk.valentin@gmail.com>
2016-02-04 14:17:21 +0100

    Clear item table on load

--- lua/ps2/modules/pointshop2/rarity/cl_DItemChanceTable.lua
+++ lua/ps2/modules/pointshop2/rarity/cl_DItemChanceTable.lua
@@ -8,13 +8,13 @@ function PANEL:Init( )
 	self.itemTable:AddColumn( "Chance" ):SetMaxWidth( 220 )
 	self.itemTable:AddColumn( "Actions" ):SetMaxWidth( 170 )
 	self.itemTable:SetDataHeight( 30 )
-	
+
 	self.bottomBar = vgui.Create( "DPanel", self )
 	self.bottomBar:Dock( BOTTOM )
 	self.bottomBar:DockMargin( 0, 5, 0, 0 )
 	self.bottomBar.Paint = function() end
 	self.bottomBar:SetTall( 40 )
-	
+
 	self.addBtn = vgui.Create( "DButton", self.bottomBar )
 	self.addBtn:SetImage( "pointshop2/plus24.png" )
 	self.addBtn.m_Image:SetSize( 16, 16 )
@@ -27,7 +27,7 @@ function PANEL:Init( )
 		frame:Center( )
 		function frame.OnFinish( frame, class, settings )
 			if not settings then return end
-			
+
 			frame:Remove( )
 			self:AddFactory( class, settings )
 		end
@@ -37,12 +37,12 @@ end
 function PANEL:GenerateChanceControl( line )
 	local pnl = vgui.Create( "DPanel", line )
 	pnl:DockPadding( 10, 3, 10, 3 )
-	
+
 	pnl.dropdown = vgui.Create( "DComboBox", pnl )
 	for k, v in ipairs( Pointshop2.Rarities ) do
 		pnl.dropdown:AddChoice( v.name, v.chance )
 	end
-	
+
 	pnl.dropdown:Dock( LEFT )
 	pnl.dropdown:SetWide( 120 )
 	pnl.dropdown:SetSkin( "Default" )
@@ -62,7 +62,7 @@ function PANEL:GenerateChanceControl( line )
 			hook.Call( "UpdateChance" )
 		end)
 	end
-	
+
 	pnl.lbl = vgui.Create( "DLabel", pnl )
 	pnl.lbl:Dock( LEFT )
 	pnl.lbl:DockMargin ( 5, 0, 0, 0 )
@@ -89,7 +89,7 @@ end
 function PANEL:GenerateActionsControl( line )
 	local pnl = vgui.Create( "DPanel", line )
 	pnl:DockPadding( 10, 3, 10, 3 )
-	
+
 	pnl.edit = vgui.Create( "DButton", pnl )
 	pnl.edit:SetText( "Configure" )
 	function pnl.edit.DoClick( )
@@ -97,17 +97,17 @@ function PANEL:GenerateActionsControl( line )
 		frame:SetSize( 800, math.Clamp( ScrH( ), 0, 768 ) )
 		frame:SetTitle( "Edit Settings" )
 		frame:SetSkin( Pointshop2.Config.DermaSkin )
-		
+
 		local ctrl = vgui.Create( line.factory:GetConfiguratorControl( ), frame )
 		ctrl:Dock( FILL )
 		ctrl:Edit( line.factory:GetLoadedSettings( ) )
-		
+
 		frame.save = vgui.Create( "DButton", frame )
 		frame.save:Dock( BOTTOM )
 		frame.save:SetText( "Save" )
 		function frame.save.DoClick( )
-			if not ctrl:GetSettingsForSave( ) then return end 
-			
+			if not ctrl:GetSettingsForSave( ) then return end
+
 			line.factory.settings = ctrl:GetSettingsForSave( )
 			line.Columns[1]:SetText( line.factory:GetShortDesc( ) )
 			frame:Remove( )
@@ -116,7 +116,7 @@ function PANEL:GenerateActionsControl( line )
 		frame:Center( )
 	end
 	pnl.edit:Dock( LEFT )
-	
+
 	pnl.remove = vgui.Create( "DButton", pnl )
 	pnl.remove:SetText( "Remove" )
 	function pnl.remove.DoClick( )
@@ -127,21 +127,21 @@ function PANEL:GenerateActionsControl( line )
 	end
 	pnl.remove:Dock( LEFT )
 	pnl.remove:DockMargin ( 5, 0, 0, 0 )
-	
+
 	return pnl
 end
 
 function PANEL:AddFactory( factoryClass, settings, chance )
 	local instance = factoryClass:new( )
 	instance.settings = settings
-	
+
 	local line = self.itemTable:AddLine( instance:GetShortDesc( ) )
 	line.Columns[2] = self:GenerateChanceControl( line )
 	line.Columns[3] = self:GenerateActionsControl( line )
 	if chance then
 		line.Columns[2]:SetChanceAmount( chance )
 	end
-	
+
 	line.factory = instance
 	if not line.factory:IsValid( ) then
 		line.Paint = function( p, w, h )
@@ -155,16 +155,17 @@ end
 function PANEL:GetSaveData( )
 	local data = { }
 	for k, v in pairs( self.itemTable:GetLines( ) ) do
-		table.insert( data, { 
-			factoryClassName = v.factory.class.name, 
+		table.insert( data, {
+			factoryClassName = v.factory.class.name,
 			factorySettings = v.factory.settings,
-			chance = v.Columns[2]:GetChanceAmount( ) 
-		} ) 
+			chance = v.Columns[2]:GetChanceAmount( )
+		} )
 	end
 	return data
 end
 
 function PANEL:LoadSaveData( data )
+	self.itemTable:Clear( )
 	for k, v in pairs( data ) do
 		local factoryClass = getClass( v.factoryClassName )
 		if not factoryClass then
