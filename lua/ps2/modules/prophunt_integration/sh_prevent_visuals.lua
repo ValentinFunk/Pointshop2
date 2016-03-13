b1adc3738e39a28b115bc069ea1aee1d3835b026
Valentin Funk <funk.valentin@gmail.com>
2016-03-13 12:21:57 +0100

    Fix a bug in PH integration

--- lua/ps2/modules/prophunt_integration/sh_prevent_visuals.lua
+++ lua/ps2/modules/prophunt_integration/sh_prevent_visuals.lua
@@ -1,8 +1,8 @@
 local function hideOnProps(ply)
-	if ply:Team() == TEAM_PROPS then
+	if ply:Team() != TEAM_HUNTERS then
 		return false
 	end
 end
-hook.Add( "PS2_VisualsShouldShow", "HideOnProps", hideOnProps )
-hook.Add( "PS2_PlayermodelShouldShow", "HideOnProps", hideOnProps )
-hook.Add( "PS2_WeaponShouldSpawn", "No weapons for props", hideOnProps )
+hook.Add( "PS2_VisualsShouldShow", "OnlyHunters", hideOnProps )
+hook.Add( "PS2_PlayermodelShouldShow", "OnlyHunters", hideOnProps )
+hook.Add( "PS2_WeaponShouldSpawn", "OnlyHunters", hideOnProps )
