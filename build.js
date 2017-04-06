7dd4e73a7078d4cc18492af374bebcc00dc6211e
Valentin Funk <funk.valentin@gmail.com>
2017-04-06 22:03:19 +0200

    Fix build zipper

--- build.js
+++ build.js
@@ -40,7 +40,7 @@ Check Installation, Guide and Developer.pdf for more information.`)
   const version = JSON.parse(readFileSync('package.json')).version
   writeFileSync('dist/pointshop2/lua/autorun/pointshop2_build.lua', `PS2_BUILD = "${version}"`)
 
-  exec('git clone git@github.com:Kamshak/LibK.git dist/libk')
+  exec('git clone https://github.com/Kamshak/LibK.git dist/libk')
   rm('-rf', 'dist/libk/.git')
 
   cp('package.json', 'dist')
