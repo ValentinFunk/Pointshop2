203f8a9af4b8206de1f5566a8c15839186b32ec3
Valentin Funk <funk.valentin@gmail.com>
2017-06-26 20:24:15 +0200

    fix(zombie-survival): Prevent items for spectators

--- lua/ps2/modules/zs_integration/sh_prevent_visuals.lua
+++ lua/ps2/modules/zs_integration/sh_prevent_visuals.lua
@@ -2,6 +2,9 @@ local function disableForZombies(ply)
 	if ply:Team() == TEAM_UNDEAD then
 		return false
 	end
+	if ply:GetObserverMode() != OBS_MODE_NONE then
+		return false
+	end
 end
 hook.Add( "PS2_PlayermodelShouldShow", "Hide; TEAM_UNDEAD", disableForZombies )
 hook.Add( "PS2_WeaponShouldSpawn", "Hide; TEAM_UNDEAD", disableForZombies )
