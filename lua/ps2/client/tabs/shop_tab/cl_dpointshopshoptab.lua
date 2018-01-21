2404c0fb95403c01eeb196f7cf4516cb830a0c2d
Valentin Funk <funk.valentin@gmail.com>
2018-01-21 21:25:16 +0100

    feat(ux): Show hint to install default items on first start

--- lua/ps2/client/tabs/shop_tab/cl_dpointshopshoptab.lua
+++ lua/ps2/client/tabs/shop_tab/cl_dpointshopshoptab.lua
@@ -5,6 +5,17 @@ function PANEL:Init( )
 	self.rightBar:Dock( RIGHT )
 	self.rightBar:SetWide( self.leftBar:GetWide( ) - 23 )
 	Derma_Hook( self.rightBar, "Paint", "Paint", "InnerPanel" )
+
+	self.loading = vgui.Create( "DLoadingNotifier", self )
+	self.loading:DockMargin( 15, 0, 15, 0 )
+	self.loading:Dock( TOP )
+
+	hook.Add( "PS2_PreReload", self.loading, function( )
+		self.loading:Expand( )
+	end )
+	hook.Add( "PS2_DynamicItemsUpdated", self.loading, function( )
+		self.loading:Collapse( )
+	end )
 	
 	self.previewPanel = vgui.Create( "DPointshopPreviewPanel", self.rightBar )
 	self.previewPanel:Dock( TOP )
@@ -68,7 +79,81 @@ local function anyItemValidForServer( category )
 	end
 end
 
+function PANEL:ShowInstall( )
+	local overlay = vgui.Create( "DPanel", self )
+	overlay:SetPos(0, 0)
+	overlay:SetZPos(100)
+	overlay:Dock(FILL)
+	Derma_Hook( overlay, "Paint", "Paint", "InnerPanelDark" )
+
+	local pnl = vgui.Create( "DPanel", overlay )
+	function pnl:PerformLayout( )
+		self:SizeToChildren( false, true )
+	end
+	pnl:Dock( TOP )
+	pnl.Paint = function( ) end
+
+	local timerLabel = vgui.Create( "DLabel", pnl )
+	timerLabel:Dock( TOP )
+	timerLabel:SetText( "Welcome to your Pointshop 2!" )
+	timerLabel:SetColor( color_white )
+	timerLabel:SetContentAlignment( 5 )
+	timerLabel:SetFont( self:GetSkin( ).BigTitleFont )
+	timerLabel:SizeToContents( )
+
+	local timerLabel2 = vgui.Create( "DLabel", pnl )
+	timerLabel2:Dock( TOP )
+	timerLabel2:SetContentAlignment( 5 )
+	timerLabel2:SetFont( self:GetSkin( ).fontName )
+	timerLabel2:SetText( "No items have been installed yet." )
+	timerLabel2:SizeToContents( )
+	
+	if PermissionInterface.query( LocalPlayer(), "pointshop2 reset" ) then
+		local btn = vgui.Create( "DButton", pnl )
+		btn:Dock( TOP )
+		btn:SetContentAlignment( 5 )
+		btn:SetFont( self:GetSkin( ).TabFont )
+		btn:SetText( "Install Default Items" )
+		btn:DockMargin( 125, 20, 125, 20)
+		btn:SetTall( 45 )
+		function btn.DoClick()
+			if btn:GetDisabled() then return end
+			btn:SetDisabled( true )
+			Pointshop2View:getInstance( ):installDefaults( )
+			btn:SetText("Installing, please wait...")
+			self.loading:Expand( )
+			timerLabel:SetText( "We are preparing your shop" )
+			timerLabel2:SetText( "This may take a minute." )
+		end
+	end
+
+	function overlay:PerformLayout( )
+		pnl:DockMargin( 0, ( self:GetTall( ) - pnl:GetTall( ) ) / 2 , 0, 0 )
+	end
+
+	self.ShowInstallPanel = overlay
+end
+
+function PANEL:PerformLayout()
+	if IsValid(self.ShowInstallPanel) then
+		self.ShowInstallPanel:SetSize( self:GetTall(), self:GetWide() )
+	else
+		DPointshopMenuedTab.PerformLayout(self)
+	end
+end
+
 function PANEL:DoPopulate( )
+	if #Pointshop2View:getInstance().itemMappings == 0 then
+		if IsValid(self.ShowInstallPanel) then self.ShowInstallPanel:Remove() end
+
+		self:ShowInstall()
+		return
+	else
+		if IsValid(self.ShowInstallPanel) then
+			self.ShowInstallPanel:Remove()
+		end
+	end
+
 	local dataNode = Pointshop2View:getInstance( ):getShopCategory( )
 	for k, category in pairs( dataNode.subcategories ) do
 		if anyItemValidForServer( category ) then
